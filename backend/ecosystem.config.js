3b4fae078e1875dc0ee8f2e483d96661f2922a33
Sergei Nesterovich <sergeinesterovich@MacBook-Pro-Sergei.local>
2024-04-16 20:22:25 +0300

    deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
