b31261dd673f6e0b50716e2f54b0b2c25c8633d5
Sergei Nesterovich <sergeinesterovich@MacBook-Pro-Sergei.local>
2024-04-16 19:51:23 +0300

    Добавляет деплой бэкенда

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,13 +1,18 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env.deploy" })
+require('dotenv').config();
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } = process.env;
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPOSITORY,
+  DEPLOY_REF,
+} = process.env;
 
 module.exports = {
   apps: [
     {
-      name: "mesto",
-      script: "dist/app.js",
+      name: 'mesto',
+      script: 'dist/app.js',
     },
   ],
   deploy: {
@@ -17,8 +22,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy": "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
     },
   },
 };
