46ad126ffdd5957da1b10fadbad1c0c57061ba1a
Sergei Nesterovich <sergeinesterovich@MacBook-Pro-Sergei.local>
2024-04-17 22:24:11 +0300

    Деплой бэкенда

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
